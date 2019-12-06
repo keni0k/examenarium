4a34874ba8c1355b1fc97873d66f5261e8afecef
Keni0k <dima-vers0@rambler.ru>
2019-12-07 05:54:53 +0700

    Admin form

--- media/views.py
+++ media/views.py
@@ -2,14 +2,8 @@ from django.shortcuts import render
 from .models import *
 
 
-# Create your views here.
-def index(request):
-    # return HttpResponse('Hello from Python!')
-    return render(request, "index.html")
-
-
 def videos(request):
-    pass
+    videos = Video.objects.filter(is_active=True)
 
 
 def images(request):
