2da33be523c306fc7120b07549327cac0fd03b87
Keni0k <dima-vers0@rambler.ru>
2019-12-07 14:34:39 +0700

    Решение конфликта миграций

--- homework/admin.py
+++ homework/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Homework)
+admin.site.register(HW)
+admin.site.register(HWResult)
+admin.site.register(HWType)
+admin.site.register(Task)
+admin.site.register(Answer)
--- homework/models.py
+++ homework/models.py
@@ -1,18 +1,16 @@
-from typing import Any
-
 from django.db import models
 from media.models import Image
+from django.contrib.auth.models import User
 
 
 class HWType(models.Model):
     name = models.CharField(max_length=100, help_text='Статус задания')
 
 
-class Homework(models.Model):
+class HW(models.Model):
     class Meta:
         verbose_name = 'Домашнее задание'
         verbose_name_plural = 'Домашние задания'
-    from django.contrib.auth.models import User
     name = models.CharField(max_length=100, help_text='Название работы')
     title = models.CharField(max_length=500, help_text='Описание работы')
     created = models.DateTimeField(auto_now_add=True)
@@ -24,12 +22,10 @@ class Task(models.Model):
     class Meta:
         verbose_name = 'Задание'
         verbose_name_plural = 'Задания'
-    from django.contrib.auth.models import User  # Required to assign User as a borrower
-    created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100, help_text='Название задания')
     text = models.CharField(max_length=500, help_text='Описание задания')
     support_image = models.ForeignKey(Image, on_delete=models.PROTECT, null=False, blank=True)
-    current_work = models.ForeignKey(Homework, on_delete=models.PROTECT, null=False, blank=True)
+    current_work = models.ForeignKey(HW, on_delete=models.PROTECT, null=False, blank=True)
     right_answer = models.CharField(max_length=500, help_text='Описание задания')
     max_result = models.IntegerField(help_text='Максимальное количество баллов за задание')
 
@@ -38,8 +34,7 @@ class HWResult(models.Model):
     class Meta:
         verbose_name = 'Результат'
         verbose_name_plural = 'Результаты'
-    from django.contrib.auth.models import User
-    finish = models.DateTimeField(auto_now_add=True)
+    finish = models.DateField(auto_now_add=True)
     result = models.ForeignKey(HWType, on_delete=models.PROTECT, null=False, blank=True)
     student = models.ForeignKey(User, on_delete=models.PROTECT, null=False, blank=True)
 
